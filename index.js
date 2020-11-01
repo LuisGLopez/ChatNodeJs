60e52747b6236b24dda05c2d34fe203e3e3918f2
LGLopez <darkdestruktion@gmail.com>
2020-10-31 23:19:57 -0600

    chat listo node js

---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let socket = require('socket.io');
+
+let app = express();
+
+let server = app.listen(4000, ()=> {
+    console.log('escuchando en el puerto 4000');
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', socket => {
+    console.log('conexion de socket', socket.id);
+
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    })
+});
---
+++ public/chat.js
@@ -0,0 +1,30 @@
+// Hacer la conexion
+let socket = io.connect('http://localhost:4000');
+
+let message = document.getElementById('message');
+let handle = document.getElementById('handle');
+let btnSend = document.getElementById('send');
+let output = document.getElementById('output');
+let feedback = document.getElementById('feedback');
+
+btnSend.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+    message.value = '';
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value);
+})
+
+// Escuchando eventos
+socket.on('chat', data => {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+})
+
+socket.on('typing', data => {
+    feedback.innerHTML = '<p><em> ' + data + 'esta escribiendo un mensaje...</em><p>';
+})
